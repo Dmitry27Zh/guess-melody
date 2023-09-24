41839865e103fd17d1777f3de86bfdbf53df31cc
Dmitry <dmitry27zh@gmail.com>
2023-09-24 11:56:46 +0300

    Create GameOverScreen

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,3 +1,4 @@
+import GameOverScreen from '../game-over-screen/game-over-screen';
 import QuestionArtistScreen from '../question-artist-screen/question-artist-screen';
 import QuestionGenreScreen from '../question-genre-screen/question-genre-screen';
 import WelcomeScreen from '../welcome-screen/welcome-screen';
@@ -7,7 +8,7 @@ type AppProps = {
 }
 
 function App({errorsCount}: AppProps): JSX.Element {
-  return <QuestionGenreScreen />;
+  return <GameOverScreen />;
 }
 
 export default App;
