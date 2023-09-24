13fe3beece3041c7e19128b15faaa0064fb31ed8
Dmitry <dmitry27zh@gmail.com>
2023-09-24 11:26:06 +0300

    Create WelcomeScreen

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,5 +1,11 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import WelcomeScreen from '../welcome-screen/welcome-screen';
+
+type AppProps = {
+  errorsCount: number;
+}
+
+function App({errorsCount}: AppProps): JSX.Element {
+  return <WelcomeScreen errorsCount={errorsCount} />;
 }
 
 export default App;
