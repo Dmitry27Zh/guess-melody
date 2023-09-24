c2296e858fd0787d92b75ee3bf50e8419960abd0
Dmitry <dmitry27zh@gmail.com>
2023-09-24 12:02:15 +0300

    Create WinScreen

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -3,13 +3,14 @@ import GameOverScreen from '../game-over-screen/game-over-screen';
 import QuestionArtistScreen from '../question-artist-screen/question-artist-screen';
 import QuestionGenreScreen from '../question-genre-screen/question-genre-screen';
 import WelcomeScreen from '../welcome-screen/welcome-screen';
+import WinScreen from '../win-screen/win-screen';
 
 type AppProps = {
   errorsCount: number;
 }
 
 function App({errorsCount}: AppProps): JSX.Element {
-  return <AuthScreen />;
+  return <WinScreen />;
 }
 
 export default App;
