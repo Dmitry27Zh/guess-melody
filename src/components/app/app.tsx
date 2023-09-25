74f86fcee1ca9189b90de26b75cb7e0ae1da619a
Dmitry <dmitry27zh@gmail.com>
2023-09-25 13:00:47 +0300

    Create const module

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,17 +1,11 @@
-import AuthScreen from '../auth-screen/auth-screen';
-import GameOverScreen from '../game-over-screen/game-over-screen';
-import NotFoundScreen from '../not-found-screen/not-found-screen';
-import QuestionArtistScreen from '../question-artist-screen/question-artist-screen';
-import QuestionGenreScreen from '../question-genre-screen/question-genre-screen';
 import WelcomeScreen from '../welcome-screen/welcome-screen';
-import WinScreen from '../win-screen/win-screen';
 
 type AppProps = {
   errorsCount: number;
 }
 
 function App({errorsCount}: AppProps): JSX.Element {
-  return <NotFoundScreen />;
+  return <WelcomeScreen errorsCount={errorsCount} />;
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
-
-const Setting = {
-  ERRORS_COUNT: 5
-};
+import { Setting } from './const';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -12,6 +9,6 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App errorsCount={Setting.ERRORS_COUNT} />
+    <App errorsCount={Setting.ErrorsCount} />
   </React.StrictMode>,
 );
