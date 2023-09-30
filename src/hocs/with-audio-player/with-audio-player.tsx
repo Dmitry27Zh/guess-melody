94dc3cdc2da1072aeb0fe9838d41f4ebfea0fa26
Dmitry <dmitry27zh@gmail.com>
2023-09-30 14:11:04 +0300

    Create withAudioPlayer HOC with render prop

---
+++ src/hocs/with-audio-player/with-audio-player.tsx
@@ -0,0 +1,30 @@
+import { ComponentType } from 'react';
+import { Id, Src } from '../../types/common';
+import {useState} from 'react';
+import AudioPlayer from '../../components/audio-player/audio-player';
+
+type HOCProps = {
+  renderPlayer: (src: Src, id: Id) => void;
+}
+
+function withAudioPlayer<T>(Component: ComponentType<T>): ComponentType<Omit<T, keyof HOCProps>> {
+  type ComponentProps = Omit<T, keyof HOCProps>
+
+  function WithAudioPlayer(props: ComponentProps):JSX.Element {
+    const [activePlayer, setActivePlayer] = useState('0');
+
+    return (
+      <Component {...props as T}
+        renderPlayer={(src: Src, id: Id) => (
+          <AudioPlayer src={src} isPlaying={id === activePlayer}
+            onPlayButtonClick={() => setActivePlayer(activePlayer === id ? '_PAUSE' : id)}
+          />
+        )}
+      />
+    );
+  }
+
+  return WithAudioPlayer;
+}
+
+export default withAudioPlayer;
