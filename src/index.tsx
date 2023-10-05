ad2f44f9af8885ce920d2e4f71aae32a832378ff
Dmitry <dmitry27zh@gmail.com>
2023-10-05 09:38:53 +0300

    Apply Redux

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,20 @@ import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 import { Setting } from './const';
 import { questions } from './mock/questions';
+import { configureStore } from '@reduxjs/toolkit';
+import { reducer } from './store/reducer';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
+const store = configureStore({
+  reducer,
+});
+
 root.render(
   <React.StrictMode>
-    <App errorsCount={Setting.ErrorsCount} questions={questions} />
+    <Provider store={store}><App errorsCount={Setting.ErrorsCount} questions={questions} /></Provider>
   </React.StrictMode>,
 );
