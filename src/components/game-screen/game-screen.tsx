84f9de4a67192dae558b8fd3f88b3f9008daf921
Dmitry <dmitry27zh@gmail.com>
2023-10-06 20:29:45 +0300

    Refactor GameScreen

--- src/components/game-screen/game-screen.tsx
+++ src/components/game-screen/game-screen.tsx
@@ -1,35 +1,36 @@
-import { Question, UserAnswer } from '../../types/question';
+import { UserAnswer } from '../../types/question';
 import QuestionArtistScreen from '../question-artist-screen/question-artist-screen';
 import QuestionGenreScreen from '../question-genre-screen/question-genre-screen';
-import { AppRoute, GameType } from '../../const';
-import { Navigate, useNavigate } from 'react-router-dom';
+import { AppRoute, GameType, MAX_MISTAKE_COUNT } from '../../const';
+import { Navigate } from 'react-router-dom';
 import withAudioPlayer from '../../hocs/with-audio-player/with-audio-player';
 import { checkAnswer, incrementStep } from '../../store/action';
 import { useAppDispatch, useAppSelector } from '../../hooks';
 import Mistakes from '../mistakes/mistakes';
 
-type GameScreenProps = {
-  questions: Question[];
-}
-
 const QuestionArtistScreenWrapped = withAudioPlayer(QuestionArtistScreen);
 const QuestionGenreScreenWrapped = withAudioPlayer(QuestionGenreScreen);
 
-function GameScreen({questions}: GameScreenProps):JSX.Element {
+function GameScreen():JSX.Element {
   const step = useAppSelector((state) => state.step);
   const mistakes = useAppSelector((state) => state.mistakes);
+  const questions = useAppSelector((state) => state.questions);
   const question = questions[step];
   const dispatch = useAppDispatch();
-  const navigate = useNavigate();
-  const onAnswer = (userAnswer: UserAnswer) => {
-    const isGameOver = step === questions.length - 1;
-    dispatch(checkAnswer(question, userAnswer));
+  const isGameOver = step === questions.length;
+  const isLost = mistakes > MAX_MISTAKE_COUNT;
 
-    if (isGameOver) {
-      navigate(AppRoute.Root);
-    } else {
-      dispatch(incrementStep());
-    }
+  if (isLost) {
+    return <Navigate to={AppRoute.Lose}></Navigate>;
+  }
+
+  if (isGameOver) {
+    return <Navigate to={AppRoute.Root}></Navigate>;
+  }
+
+  const onAnswer = (userAnswer: UserAnswer) => {
+    dispatch(checkAnswer(question, userAnswer));
+    dispatch(incrementStep());
   };
 
   switch (question.type) {
