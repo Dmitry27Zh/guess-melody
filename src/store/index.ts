38108260dd7591e950e91cc40c82e6250ca42168
Dmitry <dmitry27zh@gmail.com>
2023-10-19 11:02:24 +0300

    Apply Thunk middleware

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { reducer } from './reducer';
+import { createAPI } from '../services/api';
+
+const api = createAPI();
 
 export const store = configureStore({
   reducer,
+  middleware: (getDefaultMiddleWare) => getDefaultMiddleWare({
+    thunk: {
+      extraArgument: api
+    }
+  })
 });
