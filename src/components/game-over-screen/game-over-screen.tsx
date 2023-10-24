3c813dc7f21fe79c75bac7853beac2a07a1bd210
Dmitry <dmitry27zh@gmail.com>
2023-10-24 10:30:53 +0300

    Add replay logic in GameOverScreen

--- src/components/game-over-screen/game-over-screen.tsx
+++ src/components/game-over-screen/game-over-screen.tsx
@@ -1,6 +1,13 @@
 import { Helmet } from 'react-helmet-async';
+import { useAppDispatch } from '../../hooks';
+import { useNavigate } from 'react-router-dom';
+import { resetGame } from '../../store/action';
+import { AppRoute } from '../../const';
 
 function GameOverScreen(): JSX.Element {
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
   return (
     <section className="result">
       <Helmet>
@@ -9,7 +16,12 @@ function GameOverScreen(): JSX.Element {
       <div className="result__logo"><img src="img/melody-logo.png" alt="Угадай мелодию" width="186" height="83"/></div>
       <h2 className="result__title">Какая жалость!</h2>
       <p className="result__total result__total--fail">У вас закончились все попытки. Ничего, повезёт в следующий раз!</p>
-      <button className="replay" type="button">Попробовать ещё раз</button>
+      <button className="replay" type="button" onClick={() => {
+        dispatch(resetGame());
+        navigate(AppRoute.Game);
+      }}
+      >Попробовать ещё раз
+      </button>
     </section>
   );
 }
